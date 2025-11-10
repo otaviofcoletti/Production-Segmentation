85af8e27587e2cf85f5626073578a696e4eea233
otaviofcoletti <otaviocoletti@outlook.com>
2025-11-10 10:55:42 -0300

    adicionando arquivos de resultado

---
+++ 4-Evaluation/README.md
@@ -0,0 +1,3 @@
+### Evaluation
+
+#### Aqui há apenas alguns arquivos para gerar algumas sobreposicoes e avaliar o modelo, como não é algo que está dentro dos requerimentos ele não foi organizado devido a priorização de tempo para o bom desenvolvimento dos requisitos.
--- 4-Evaluation/main.py
+++ 4-Evaluation/main.py
@@ -4,9 +4,9 @@ from tiling import dividir_treino_teste_geotiff
 from tiling import sobrepor_mascara_verde
 
 
-# reconstruir_geotiff("output_masks", "reconstrucao_masks_teste.tif", tile_size=256, overlap=0.0)
-# sobrepor_mascara_verde("plantacao_teste.tif", "reconstrucao_masks_teste.tif", "sobreposicao.tif", alpha=0.4)
+reconstruir_geotiff("output_masks", "reconstrucao_masks_teste.tif", tile_size=256, overlap=0.0)
+sobrepor_mascara_verde("plantacao_teste.tif", "reconstrucao_masks_teste.tif", "sobreposicao0.tif", alpha=0.4)
 
 
-reconstruir_geotiff("output_masks_plantacao2", "reconstrucao_masks_plantacao2.tif", tile_size=256, overlap=0.0)
-sobrepor_mascara_verde("plantacao2.png", "reconstrucao_masks_plantacao2.tif", "sobreposicao.tif", alpha=0.4)
+# reconstruir_geotiff("output_masks_plantacao2", "reconstrucao_masks_plantacao2.tif", tile_size=256, overlap=0.0)
+# sobrepor_mascara_verde("plantacao2.png", "reconstrucao_masks_plantacao2.tif", "sobreposicao.tif", alpha=0.4)
